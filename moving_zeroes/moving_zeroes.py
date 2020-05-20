949764507b0c6a66fb24f87c669e9908190cb9c8
Mike-Xie <Mike-Xie@users.noreply.github.com>
2020-05-20 16:27:42 -0700

    fix sorting zeroes single pass using swaps

--- moving_zeroes/moving_zeroes.py
+++ moving_zeroes/moving_zeroes.py
@@ -2,14 +2,22 @@
 Input: a List of integers
 Returns: a List of integers
 '''
-def moving_zeroes(arr):
-    # Your code here
+def moving_zeroes(arr: list):
+    index: int = 0
 
-    pass
+    for i in range(len(arr)):
+    	elem = arr[i]
+    	if elem != 0:
+    		arr[i], arr[index] = arr[index], arr[i]
+    		index += 1 
 
+
+    return arr 
 
 if __name__ == '__main__':
     # Use the main function here to test out your implementation
-    arr = [0, 3, 1, 0, -2]
+    arr = [0, 0, 0, 0, 3, 2, 1]
+    # [0, 3, 1, 0, -2]
+    
 
     print(f"The resulting of moving_zeroes is: {moving_zeroes(arr)}")
