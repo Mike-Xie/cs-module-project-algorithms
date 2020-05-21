65387a7f06a2676bee69f628199f7784754395a1
Mike-Xie <Mike-Xie@users.noreply.github.com>
2020-05-20 20:56:29 -0700

    add comments

--- moving_zeroes/moving_zeroes.py
+++ moving_zeroes/moving_zeroes.py
@@ -3,8 +3,10 @@ Input: a List of integers
 Returns: a List of integers
 '''
 def moving_zeroes(arr: list):
+	# since beginning must be non zero, we 
     index: int = 0
-
+    # iterate thru list, if we find a non zero element
+    # we swap it to index, which is the beginning then increment index
     for i in range(len(arr)):
     	elem = arr[i]
     	if elem != 0:
