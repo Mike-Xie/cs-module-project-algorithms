c6e111bec1d6eef516df4b3fae610e5867aefce5
Mike-Xie <Mike-Xie@users.noreply.github.com>
2020-05-21 16:06:48 -0700

    one pass of single number keep all tallies

--- single_number/single_number.py
+++ single_number/single_number.py
@@ -2,10 +2,19 @@
 Input: a List of integers where every int except one shows up twice
 Returns: an integer
 '''
-def single_number(arr):
-    # Your code here
+def single_number(arr: list):
+    # store tallies in dict object
+    counts: dict = {}
+    # increment tallies
+    for i in arr:
+        counts[i] = counts.get(i, 0) + 1
+    # return single ton 
+    for v in counts:
+        if counts[v] == 1:
+            return v
 
-    pass
+    print(counts) 
+
 
 
 if __name__ == '__main__':
